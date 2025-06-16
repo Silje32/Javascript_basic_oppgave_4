9b622e3c4d65ff129562f15eeed236144dd18080
Silje32 <msh070425@gmail.com>
2025-06-16 11:00:39 +0200

    Added my array of objects

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Array with objects */
+const card = [
+    {
+        Stage: "1",
+        Img: "eggs.jpg",
+        Facts: "Female butterfly lays egg on a plant. The egg is generaly round or oval in shape.",
+    }
+    {
+        Stage: "2",
+        Img: "larva.jpg",
+        Facts: "The egg haches into a larva, commonly known as a caterpillar. The caterpillar need to eat a lot of foods to grow. They start to eat the plant they hached on. They grow so big from eating that they shed their skin four times before fully developing and moving into the third stage.",
+   } 
+   {
+        Stage: "3",
+        Img: "chrysalis.jpg",
+        Facts: "The caterpillar is fully grown and forms a cocoon, also called a chrysalis. Inside the chrysalis, the caterpillar hibernates and transform into a butterfly",
+ }
+ {
+        Stage: "4",
+        Img: "butterfly.jpg",
+        Facts: "The chrysalis opens to reveal a colorful butterfly. The butterfly takes 3-4 hours to learn to fly",
+ }
+];
+
