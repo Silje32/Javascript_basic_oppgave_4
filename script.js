ba62ed054e9f51ab4b01bc060b0cbb0fb18380b2
Silje32 <msh070425@gmail.com>
2025-06-16 11:38:20 +0200

    Fixed a bug

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 /* Array with objects */
 const cards = [
-    {
-        Stage: "1",
-        Img: "eggs.jpg",
-        Facts: "Female butterfly lays egg on a plant. The egg is generaly round or oval in shape.",
-    }
-    {
-        Stage: "2",
-        Img: "larva.jpg",
-        Facts: "The egg haches into a larva, commonly known as a caterpillar. The caterpillar need to eat a lot of foods to grow. They start to eat the plant they hached on. They grow so big from eating that they shed their skin four times before fully developing and moving into the third stage.",
-   } 
-   {
-        Stage: "3",
-        Img: "chrysalis.jpg",
-        Facts: "The caterpillar is fully grown and forms a cocoon, also called a chrysalis. Inside the chrysalis, the caterpillar hibernates and transform into a butterfly",
- }
- {
-        Stage: "4",
-        Img: "butterfly.jpg",
-        Facts: "The chrysalis opens to reveal a colorful butterfly. The butterfly takes 3-4 hours to learn to fly",
- }
+  {
+    Stage: "1",
+    Img: "eggs.jpg",
+    Facts:
+      "Female butterfly lays egg on a plant. The egg is generaly round or oval in shape.",
+  },
+  {
+    Stage: "2",
+    Img: "larva.jpg",
+    Facts:
+      "The egg haches into a larva, commonly known as a caterpillar. The caterpillar need to eat a lot of foods to grow. They start to eat the plant they hached on. They grow so big from eating that they shed their skin four times before fully developing and moving into the third stage.",
+  },
+  {
+    Stage: "3",
+    Img: "chrysalis.jpg",
+    Facts:
+      "The caterpillar is fully grown and forms a cocoon, also called a chrysalis. Inside the chrysalis, the caterpillar hibernates and transform into a butterfly",
+  },
+  {
+    Stage: "4",
+    Img: "butterfly.jpg",
+    Facts:
+      "The chrysalis opens to reveal a colorful butterfly. The butterfly takes 3-4 hours to learn to fly",
+  },
 ];
 
 /* Create the container */
@@ -29,16 +33,12 @@ document.body.appendChild("container");
 
 /* Create the loop */
 for (let card of cards) {
-const butterflyCard = document.createElement("div");
-card.classList.add("butterflyCard");
-container.append(butterflyCard);
+  const butterflyCard = document.createElement("div");
+  card.classList.add("butterflyCard");
+  container.append(butterflyCard);
 
-/* Create the h2 */
-const stage = document.createElement("h2");
-stage.textContent = card.Stage;
-butterflyCard.append(stage);
-};
-
-
-
-
+  /* Create the h2 */
+  const stage = document.createElement("h2");
+  stage.textContent = card.Stage;
+  butterflyCard.append(stage);
+}
