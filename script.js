e9e7ef8dcf676504175688e6c989eba2366aa381
Silje32 <msh070425@gmail.com>
2025-06-16 15:28:32 +0200

    Fixed a bug in my array of objects

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 /* Array with objects */
 const cards = [
   {
-    stage: "1",
+    stage: 1,
     facts:
       "Female butterfly lays egg on a plant. The egg is generaly round or oval in shape.",
   },
   {
-    stage: "2",
+    stage: 2,
     facts:
       "The egg haches into a larva, commonly known as a caterpillar. The caterpillar need to eat a lot of foods to grow. They start to eat the plant they hached on. They grow so big from eating that they shed their skin four times before fully developing and moving into the third stage.",
   },
   {
-    stage: "3",
+    stage: 3,
     facts:
       "The caterpillar is fully grown and forms a cocoon, also called a chrysalis. Inside the chrysalis, the caterpillar hibernates and transform into a butterfly",
   },
   {
-    stage: "4",
+    stage: 4,
     facts:
       "The chrysalis opens to reveal a colorful butterfly. The butterfly takes 3-4 hours to learn to fly",
   },
@@ -36,5 +36,5 @@ for (let card of cards) {
   /* Create the h2 */
   const title = document.createElement("h2");
   title.textContent = cards.stage;
-  butterflyCard.append("title");
+  butterflyCard.append(title);
 }
