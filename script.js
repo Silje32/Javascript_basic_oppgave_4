a31fd6df30c644075de91504d7b08bd66adc6249
Silje32 <msh070425@gmail.com>
2025-06-16 12:28:07 +0200

    Fixed a bug

--- script.js
+++ script.js
@@ -2,25 +2,21 @@
 const cards = [
   {
     Stage: "1",
-    Img: "eggs.jpg",
     Facts:
       "Female butterfly lays egg on a plant. The egg is generaly round or oval in shape.",
   },
   {
     Stage: "2",
-    Img: "larva.jpg",
     Facts:
       "The egg haches into a larva, commonly known as a caterpillar. The caterpillar need to eat a lot of foods to grow. They start to eat the plant they hached on. They grow so big from eating that they shed their skin four times before fully developing and moving into the third stage.",
   },
   {
     Stage: "3",
-    Img: "chrysalis.jpg",
     Facts:
       "The caterpillar is fully grown and forms a cocoon, also called a chrysalis. Inside the chrysalis, the caterpillar hibernates and transform into a butterfly",
   },
   {
     Stage: "4",
-    Img: "butterfly.jpg",
     Facts:
       "The chrysalis opens to reveal a colorful butterfly. The butterfly takes 3-4 hours to learn to fly",
   },
@@ -39,6 +35,6 @@ for (let card of cards) {
 
   /* Create the h2 */
   const stage = document.createElement("h2");
-  stage.textContent = card.Stage;
+  stage.textContent = "card.Stage";
   butterflyCard.append(stage);
 }
