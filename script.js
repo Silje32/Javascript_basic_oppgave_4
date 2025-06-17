0b95498ec74c5c4d1dba5d3555f9661280dd05a2
Silje32 <msh070425@gmail.com>
2025-06-17 10:48:19 +0200

    Added img to my array of objects and removed the hardcoding in the code

--- script.js
+++ script.js
@@ -2,23 +2,27 @@
 const cards = [
   {
     stage: 1,
+    img: "./images/eggs.jpg",
     facts:
       "Female butterfly lays egg on a plant. The egg is generaly round or oval in shape.",
   },
   {
     stage: 2,
+    img: "./images/caterpillar.jpg",
     facts:
       "The egg haches into a larva, commonly known as a caterpillar. The caterpillar need to eat a lot of foods to grow. They start to eat the plant they hached on. They grow so big from eating that they shed their skin four times before fully developing and moving into the third stage.",
   },
   {
     stage: 3,
+    img: "./images/chrysalis.jpg",
     facts:
       "The caterpillar is fully grown and forms a cocoon, also called a chrysalis. Inside the chrysalis, the caterpillar hibernates and transform into a butterfly",
   },
   {
     stage: 4,
+    img: "./images/butterfly.jpg",
     facts:
-      "The chrysalis opens to reveal a colorful butterfly. The butterfly takes 3-4 hours to learn to fly",
+      "The chrysalis opens to reveal a colorful butterfly. The butterfly takes 3-4 hours to learn to fly.",
   },
 ];
 
@@ -40,8 +44,12 @@ for (let card of cards) {
 
   /* Create the img */
   const img = document.createElement("img");
-  img.setAttribute("src", "./images/eggs.jpg");
+  img.src = `${card.img}`;
+  img.alt = `${card.img}`;
   container.appendChild(img);
+
+  // img.setAttribute("src", "./images/eggs.jpg");
+  // container.appendChild(img);
 
   /* Create the facts */
   const facts = document.createElement("p");
